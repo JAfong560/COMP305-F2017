4f8505fa4c0856d5df245b497a2bbdb7cd9f3eba
JAfong560 <jonny560a@hotmail.com>
2017-09-13 23:47:22 -0400

    Lab2

---
+++ COMP305-F2017/Assets/Scripts/click_listener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click_listener : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse down");
+    }
+
+    //void OnMouseOver()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //    }
+    //}
+}
